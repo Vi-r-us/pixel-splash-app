ef38a7dccfe876ed8108294f4864805dfa3b2505
Sahil Kumar <sahil23012000@gmail.com>
2024-02-22 20:44:08 +0530

    edit: fixed the env issue

--- src/context.jsx
+++ src/context.jsx
@@ -4,7 +4,6 @@ import { createContext, useContext, useEffect, useState } from "react";
 import { getUserPreferenceTheme } from "./utils";
 
 const url = `https://api.unsplash.com/search/photos?client_id=${import.meta.env.VITE_API_KEY}`;
-console.log(import.meta.env.VITE_API_KEY);
 
 const AppContext = createContext();
 
